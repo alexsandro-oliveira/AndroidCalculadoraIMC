7aade4c92b167fd83ba9caf9a9944886f9008837
Alexs <ale.osantos@hotmail.com>
2024-08-22 21:26:36 +0100

    feat: add empty field validation

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -16,11 +17,26 @@ class MainActivity : AppCompatActivity() {
         val btnCalc = findViewById<Button>(R.id.btn_calc)
 
         btnCalc.setOnClickListener {
-            val peso: Float = edtWeight.text.toString().toFloat()
-            val altura: Float = edtHeight.text.toString().toFloat()
 
-            val imc = peso / (altura*2)
-            println(imc)
+            val pesoStr: String = edtWeight.text.toString()
+            val alturaStr: String = edtHeight.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+
+                Snackbar.make(
+                    edtWeight,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                ).show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val imc = peso / (altura * 2)
+
+                println(imc)
+            }
 
 
         }
